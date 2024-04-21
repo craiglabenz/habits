14c55775c15d66124b60c0b08460f8a1b60203e1
Craig Labenz <craig.labenz@gmail.com>
2024-04-21 12:48:45 -0700

    adds common_types package

---
+++ packages/common_types/lib/src/common_types_base.dart
@@ -0,0 +1,11 @@
+/// Data type for POST requests.
+typedef Body = Map<String, Object?>;
+
+/// Data type for GET requests.
+typedef Params = Map<String, Object?>;
+
+/// Data type for serialized records.
+typedef Json = Map<String, Object?>;
+
+/// Data type for HTTP request meta data.
+typedef Headers = Map<String, String>;
